14faf522d9fffb5d4deef7ee0ffb74f8f5331b4f
Samir Tapiero <samtapiero@gmail.com>
2024-02-23 22:27:54 -0500

    :camera: coming soon

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+import animations from '@midudev/tailwind-animations'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#ddd",
+				secondary: "#333"
+			}
+		},
+	},
+	plugins: [animations],
+}
